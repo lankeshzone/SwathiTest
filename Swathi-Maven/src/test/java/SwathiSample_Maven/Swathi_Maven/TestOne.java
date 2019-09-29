6989778fdf34170ad409a18226ed988dc88b03e9
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2019-09-29 17:02:05 +0530

    Applying my changes to server...

--- Swathi-Maven/src/test/java/SwathiSample_Maven/Swathi_Maven/TestOne.java
+++ Swathi-Maven/src/test/java/SwathiSample_Maven/Swathi_Maven/TestOne.java
@@ -2,9 +2,11 @@ package SwathiSample_Maven.Swathi_Maven;
 
 public class TestOne {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+	
+		//This is my local code
+		
 	}
 
 }
