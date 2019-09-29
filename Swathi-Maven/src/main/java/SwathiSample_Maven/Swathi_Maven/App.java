8edd79331562fd6a60dbfceed87d7e38fa21d0a8
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2019-09-29 16:53:49 +0530

    Initial project created

---
+++ Swathi-Maven/src/main/java/SwathiSample_Maven/Swathi_Maven/App.java
@@ -0,0 +1,13 @@
+package SwathiSample_Maven.Swathi_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
